3520e1dfb293125fbfeaf190c7b1df7d66a9fb59
shanusj3 <shanujosephsj3@gmail.com>
2023-10-25 16:21:09 +0530

    first commit

---
+++ backend/dist/index.js
@@ -0,0 +1,11 @@
+import express from "express";
+const app = express();
+app.use(express.json());
+app.post('/hello', (req, res, next) => {
+    res.send('Hello');
+    console.log(req.body.name);
+});
+app.listen(5000, () => {
+    console.log('Server is running');
+});
+//# sourceMappingURL=index.js.map
